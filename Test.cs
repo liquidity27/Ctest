4c97c8320a4f7091a0c079649551d1170a8ec07d
liquidity27 <liquidityfirst@gmail.com>
2020-10-21 00:55:47 +0900

    add new file

---
+++ Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] points = { 83, 99, 52, 93, 15 };
+
+        int sum = 0;
+        for (int i = 0; i< points.Length; i++)
+        {
+            sum += points[i];
+        }
+        int average = sum / points.Length;
+        Debug.Log(average);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
